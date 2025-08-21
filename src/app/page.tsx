d06a4ffdb7daf15c843ca8e5425118786195cbc9
mohammed imtiyaz <mohammedimtiyaz.jk@yahoo.com>
2025-08-21 20:21:29 +0530

    setup for trpc and tanstack

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,20 @@
-export default async function Home() {
-  return <div className="font-bold ">Hellow warald</div>;
+"use client";
+
+import { useTRPC } from "@/trpc/client";
+import { useSuspenseQuery } from "@tanstack/react-query";
+
+export default function Client() {
+  const trpc = useTRPC();
+  const { data } = useSuspenseQuery(
+    trpc.hello.queryOptions({
+      text: "World",
+    })
+  );
+
+  return (
+    <div>
+      <h1>Client</h1>
+      <p>{data.greeting}</p>
+    </div>
+  );
 }
